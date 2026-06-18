31859775e80d3d3896f3b38be406bac55a2ed30d
officialatrail <officialatrail@gmail.com>
2026-06-18 02:40:16 +0100

    v4

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  base: '/atrail-site/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/atrail-site/' : '/',
   plugins: [react()],
   server: {
     allowedHosts: true,
@@ -40,4 +40,4 @@ export default defineConfig({
       },
     },
   },
-});
+}));
